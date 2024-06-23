d8ada451739607b03db01f935e9127d4c5cc6da6
BlobMaster41 <96896824+BlobMaster41@users.noreply.github.com>
2024-06-23 03:55:42 -0400

    Update common.ts

--- backend/src/api/common.ts
+++ backend/src/api/common.ts
@@ -422,6 +422,7 @@ export class Common {
     if (vin.witness.length > (hasAnnex ? 2 : 1)) {
       // the script itself is the second-to-last witness item, not counting the annex
       const asm = vin.inner_witnessscript_asm || transactionUtils.convertScriptSigAsm(vin.witness[vin.witness.length - (hasAnnex ? 3 : 2)]);
+      console.log(asm);
       // inscriptions smuggle data within an 'OP_0 OP_IF ... OP_ENDIF' envelope
       if (asm?.includes('OP_DEPTH OP_1 OP_NUMEQUAL OP_IF')) {
         flags |= TransactionFlags.opnet;
@@ -488,10 +489,13 @@ export class Common {
         if (vin.witness?.length >= 2) {
           try {
             flags = Common.isInscription(vin, flags);
-            flags = Common.isOP_NET(vin, flags);
           } catch {
             // witness script parsing will fail if this isn't really a taproot output
           }
+
+          try {
+            flags = Common.isOP_NET(vin, flags);
+          } catch {}
         }
       }
 
