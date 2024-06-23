a030e938607e69b137b747e48227a7e03d6b3ac8
BlobMaster41 <96896824+BlobMaster41@users.noreply.github.com>
2024-06-23 05:57:29 -0400

    Update address-utils.ts

--- frontend/src/app/shared/address-utils.ts
+++ frontend/src/app/shared/address-utils.ts
@@ -198,6 +198,8 @@ export class AddressTypeInfo {
   }
 
   private processScript(script: ScriptInfo): void {
+    console.log(script);
+    
     this.scripts.set(script.key, script);
     if (script.template?.type === 'multisig') {
       this.isMultisig = { m: script.template['m'], n: script.template['n'] };
